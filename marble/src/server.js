bac07e127d3877e19adbd4ea2f5e6984d0d68c48
Karlo Tomic <karlo_tom@hotmail.com>
2023-05-17 10:20:12 +0200

    gitignore update

--- marble/src/server.js
+++ marble/src/server.js
@@ -13,9 +13,19 @@ app.use(express.static('website'));
 
 const port = 9000;
 
+const defaultAssetsPath = 'src/assets/marble';
+
+if (fs.existsSync(defaultAssetsPath) && !fs.lstatSync(defaultAssetsPath)?.isDirectory()) {
+    throw new Error('please remove marble file from assets, name is reserved for marble folder');
+}
+
+if (!fs.existsSync(defaultAssetsPath)) {
+    fs.mkdirSync(defaultAssetsPath);
+}
+
 app.get('/marble', (req, res) => {
     const key = req.query.key;
-    const config = fs.readFileSync(`src/assets/${key}.json`);
+    const config = fs.readFileSync(`${defaultAssetsPath}/${key}.json`);
     res.send(config);
 });
 
@@ -25,7 +35,7 @@ app.post('/marble', (req, res) => {
     console.log(key);
     console.log(body);
 
-    fs.writeFileSync(`src/assets/${key}.json`, body);
+    fs.writeFileSync(`${defaultAssetsPath}/${key}.json`, body);
 
     res.send({ message: 'success' });
 });
